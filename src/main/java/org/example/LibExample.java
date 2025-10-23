1c61e6de45ab272f57eccbd8ded80b60557ccd61
Aleksandra Zakharova <lastlight1998@gmail.com>
2025-10-23 16:43:49 +0300

    comment usage of local lib

--- src/main/java/org/example/LibExample.java
+++ src/main/java/org/example/LibExample.java
@@ -1,11 +1,11 @@
 package org.example;
 
-import sasha.example.LogUtils;
+//import sasha.example.LogUtils;
 
 public class LibExample {
 
     public static void main(String[] args) {
-        String debugMessage = LogUtils.getDebugMessage("message from JavaExamples");
-        System.out.println(debugMessage);
+//        String debugMessage = LogUtils.getDebugMessage("message from JavaExamples");
+//        System.out.println(debugMessage);
     }
 }
